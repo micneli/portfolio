98511eacf644ac424d9230e820b872012a66cd8c
micneli <monte.nemi@gmail.com>
2020-02-16 20:08:37 +0100

    added portfolio tab content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 $('aside').localScroll();
+
+// $('#tabs').tabs({hide: 'fade', show: 'fade'});
+
+$(document).ready(function(){
+
+  $('.tabs-menu a').click(function(event) {
+    event.preventDefault();
+    
+    // Toggle active class on tab buttons
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
+    
+    // display only active tab content
+    var activeTab = $(this).attr("href");
+    $('.tab-content').not(activeTab).css("display","none");
+    $(activeTab).fadeIn();
+  
+  });
+  
+});
